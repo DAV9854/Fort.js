ba18f66996d8b075778043707631ef52c0ae59c6
Idris Khenchil <idris@colourity.com>
2014-06-29 13:44:26 -0400

    Removed whitespace

--- fort.js
+++ fort.js
@@ -134,7 +134,4 @@ document.querySelector(".colors").style.background = generateCSSGradient(cols);
 var window_width = window.innerWidth + "px";
 document.querySelector(".colors").style.width = window_width;
 
-
-
-
 };
