4f4e1110fcfa586ac84671fe72b12d9bcc8632c6
Idris <idriskhenchil@gmail.com>
2014-12-02 16:21:03 -0500

    Update fort.js

--- demo/sections/fort.js
+++ demo/sections/fort.js
@@ -1,101 +1,142 @@
 function solid(){
-function cback(e) {
-    var t = [];
-    for (var n = inputs.length; n--;) {
-        if (!inputs[n].value.length) t.push(inputs[n]);
+    function cback(e) {
+        var t = [];
+        for (var n = inputs.length; n--;) {
+            if (!inputs[n].value.length) t.push(inputs[n]);
+        }
+        var r = t.length;
+        var i = inputs.length;
+        var s = document.querySelectorAll(".top");
+        for (var o = s.length; o--;) {
+            s[o].style.width = 100 - r / i * 100 + "%";
+        }
     }
-    var r = t.length;
-    var i = inputs.length;
-    var s = document.querySelectorAll(".top");
-    for (var o = s.length; o--;) {
-        s[o].style.width = 100 - r / i * 100 + "%";
+    var forms = document.querySelectorAll(".form"),
+    inputs = [];
+    for (var i = forms.length; i--;) {
+        var els = forms[i].querySelectorAll("input, textarea, select");
+        for (var j = els.length; j--;) {
+          classes = els[j].className.replace(/\s+/g, ' ').split(' ');
+          ignore = false;
+          for (var k = classes.length; k--;)
+          {
+           if (classes[k] == "ignore")
+           {
+            ignore = true;
+            break;
+        }
+    }
+    if (els[j].type != "button" && els[j].type != "submit" && !ignore) {
+        inputs.push(els[j]);
+        els[j].addEventListener("input", cback, false);
     }
 }
-var forms = document.querySelectorAll(".form"),
+}
+}
+function flash(){
+    function cback(e) {
+        var t = [];
+        for (var n = inputs.length; n--;) {
+            if (!inputs[n].value.length) t.push(inputs[n]);
+        }
+        var r = t.length;
+        var i = inputs.length;
+        var s = document.querySelectorAll(".top");
+        for (var o = s.length; o--;) {
+            s[o].style.width = 100 - r / i * 100 + "%";
+            s[o].style.background = cols[i-r-1];
+        }
+    }
+    var forms = document.querySelectorAll(".form"),
     inputs = [];
-for (var i = forms.length; i--;) {
-    var els = forms[i].querySelectorAll("input, textarea, select");
-    for (var j = els.length; j--;) {
-        if (els[j].type != "button" && els[j].type != "submit") {
+    for (var i = forms.length; i--;) {
+        var els = forms[i].querySelectorAll("input, textarea, select");
+        for (var j = els.length; j--;) {
+            classes = els[j].className.replace(/\s+/g, ' ').split(' ');
+            ignore = false;
+            for (var k = classes.length; k--;)
+            {
+               if (classes[k] == "ignore")
+               {
+                ignore = true;
+                break;
+            }
+        }
+        if (els[j].type != "button" && els[j].type != "submit" && !ignore) {
+            inputs.push(els[j]);
+            els[j].addEventListener("input", cback, false);
+        }
+
+    }
+}
+//Edit colors here
+var cols = ["#1ABC9C","#EC7063","#3498DB"];
+}
+
+function gradient(){
+    function cback(e) {
+        var t = [];
+        for (var n = inputs.length; n--;) {
+            if (!inputs[n].value.length) t.push(inputs[n]);
+        }
+        var r = t.length;
+        var i = inputs.length;
+        var s = document.querySelectorAll(".top");
+        for (var o = s.length; o--;) {
+            s[o].style.width = 100 - r / i * 100 + "%";
+        }
+    }
+    var forms = document.querySelectorAll(".form"),
+    inputs = [];
+    for (var i = forms.length; i--;) {
+        var els = forms[i].querySelectorAll("input, textarea, select");
+        for (var j = els.length; j--;) {
+            classes = els[j].className.replace(/\s+/g, ' ').split(' ');
+            ignore = false;
+            for (var k = classes.length; k--;)
+            {
+               if (classes[k] == "ignore")
+               {
+                ignore = true;
+                break;
+            }
+        }
+        if (els[j].type != "button" && els[j].type != "submit" && !ignore) {
             inputs.push(els[j]);
             els[j].addEventListener("input", cback, false);
         }
     }
 }
 }
-function flash(){
-function cback(e) {
-var t = [];
-for (var n = inputs.length; n--;) {
-    if (!inputs[n].value.length) t.push(inputs[n]);
-}
-var r = t.length;
-var i = inputs.length;
-var s = document.querySelectorAll(".top");
-for (var o = s.length; o--;) {
-    s[o].style.width = 100 - r / i * 100 + "%";
-    s[o].style.background = cols[i-r-1];
-}
-}
-var forms = document.querySelectorAll(".form"),
-inputs = [];
-for (var i = forms.length; i--;) {
-var els = forms[i].querySelectorAll("input, textarea, select");
-for (var j = els.length; j--;) {
-    if (els[j].type != "button" && els[j].type != "submit") {
-        inputs.push(els[j]);
-        els[j].addEventListener("input", cback, false);
-    }
-}
-}
-
-var cols = ["#1ABC9C","#EC7063","#3498DB"];
-}
-
-function gradient(){
-function cback(e) {
-var t = [];
-for (var n = inputs.length; n--;) {
-    if (!inputs[n].value.length) t.push(inputs[n]);
-}
-var r = t.length;
-var i = inputs.length;
-var s = document.querySelectorAll(".top");
-for (var o = s.length; o--;) {
-    s[o].style.width = 100 - r / i * 100 + "%";
-}
-}
-var forms = document.querySelectorAll(".form"),
-inputs = [];
-for (var i = forms.length; i--;) {
-var els = forms[i].querySelectorAll("input, textarea, select");
-for (var j = els.length; j--;) {
-    if (els[j].type != "button" && els[j].type != "submit") {
-        inputs.push(els[j]);
-        els[j].addEventListener("input", cback, false);
-    }
-}
-}
-}
 function sections(){
-function cback(e) {
-var t = [];
-for (var n = inputs.length; n--;) {
-    if (!inputs[n].value.length) t.push(inputs[n]);
-}
-var r = t.length;
-var i = inputs.length;
-var s = document.querySelectorAll(".top");
-for (var o = s.length; o--;) {
-    s[o].style.width = 100 - r / i * 100 + "%";
-}
-}
-var forms = document.querySelectorAll(".form"),
-inputs = [];
-for (var i = forms.length; i--;) {
-var els = forms[i].querySelectorAll("input, textarea, select");
-for (var j = els.length; j--;) {
-    if (els[j].type != "button" && els[j].type != "submit") {
+    function cback(e) {
+        var t = [];
+        for (var n = inputs.length; n--;) {
+            if (!inputs[n].value.length) t.push(inputs[n]);
+        }
+        var r = t.length;
+        var i = inputs.length;
+        var s = document.querySelectorAll(".top");
+        for (var o = s.length; o--;) {
+            s[o].style.width = 100 - r / i * 100 + "%";
+        }
+    }
+    var forms = document.querySelectorAll(".form"),
+    inputs = [];
+    for (var i = forms.length; i--;) {
+        var els = forms[i].querySelectorAll("input, textarea, select");
+        for (var j = els.length; j--;) {
+         classes = els[j].className.replace(/\s+/g, ' ').split(' ');
+         ignore = false;
+         for (var k = classes.length; k--;)
+         {
+           if (classes[k] == "ignore")
+           {
+            ignore = true;
+            break;
+        }
+    }
+    if (els[j].type != "button" && els[j].type != "submit" && !ignore) {
         inputs.push(els[j]);
         els[j].addEventListener("input", cback, false);
     }
@@ -104,9 +145,9 @@ for (var j = els.length; j--;) {
 
 
 function generateCSSGradient(colours) {
-var l = colours.length, i;
-for( i=0; i<l; i++) colours[i] = colours[i].join(" ");
-return "linear-gradient( to right, "+colours.join(", ")+")";
+    var l = colours.length, i;
+    for( i=0; i<l; i++) colours[i] = colours[i].join(" ");
+        return "linear-gradient( to right, "+colours.join(", ")+")";
 }
 
 //Edit your colors here. Enter the color twice.
@@ -123,4 +164,5 @@ document.querySelector(".colors").style.background = generateCSSGradient(cols);
 
 var window_width = window.innerWidth + "px";
 document.querySelector(".colors").style.width = window_width;
+
 };
